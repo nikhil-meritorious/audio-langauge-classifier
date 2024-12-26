dd1f00252cfa7e589d54e255d0539493f7b3836c
nikhil-meritorious <nikhil.meritorious@gmail.com>
2024-12-26 16:40:12 +0530

    feature shuffle before storing into csv

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -14,21 +14,18 @@ def extract_features(directory: str) -> pd.DataFrame | None:
             for audio_file in os.listdir(f'{directory}/{accent}'):
                 if audio_file.endswith('.wav') or audio_file.endswith('.mp3'):
                     file_path = os.path.join(f'{directory}/{accent}', audio_file)
-
                     audio, sample_rate = librosa.load(file_path, sr=22050)
 
                     mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
                     mfcc_mean = np.mean(mfccs.T, axis=0)
-
                     spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=sample_rate)[0])
-                    zero_crossing_rate = np.mean(librosa.feature.zero_crossing_rate(audio)[0])
-                    
+                    zero_crossing_rate = np.mean(librosa.feature.zero_crossing_rate(audio)[0])                  
                     spectral_contrast_mean = np.mean(librosa.feature.spectral_contrast(y=audio, sr=sample_rate, n_bands=5), axis=1)
-
                     chroma_mean = np.mean(librosa.feature.chroma_stft(y=audio, sr=sample_rate), axis=1)
-
                     rms = np.mean(librosa.feature.rms(y=audio)[0])
                     spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=audio, sr=sample_rate))
+                    mel_spectrogram = np.mean(librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels=128), axis=1)
+                    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(audio), sr=sample_rate), axis=1)
 
                     temp_features = {
                         "file": audio_file,
@@ -38,7 +35,6 @@ def extract_features(directory: str) -> pd.DataFrame | None:
                         "rms": rms,
                         "spectral_bandwidth": spectral_bandwidth
                     }
-
                         
                     for index, mfcc in enumerate(mfcc_mean):
                         temp_features[f'mfcc{index + 1}'] = mfcc
@@ -48,13 +44,9 @@ def extract_features(directory: str) -> pd.DataFrame | None:
 
                     for index, spectral_contrast in enumerate(spectral_contrast_mean):
                         temp_features[f'spectral_contrast{index + 1}'] = spectral_contrast
-                    
-                    mel_spectrogram = np.mean(librosa.feature.melspectrogram(y=audio, sr=sample_rate, n_mels=128), axis=1)
-                    
+                                        
                     for index, mel in enumerate(mel_spectrogram):
                         temp_features[f'mel{index + 1}'] = mel
-
-                    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(audio), sr=sample_rate), axis=1)
                     
                     for index, tonnetz_value in enumerate(tonnetz):
                         temp_features[f'tonnetz{index + 1}'] = tonnetz_value
@@ -69,6 +61,6 @@ def extract_features(directory: str) -> pd.DataFrame | None:
 
 if __name__ == '__main__':
     feature_df = extract_features('data/audio_samples')
-    # feature_df = shuffle(feature_df)
+    feature_df = shuffle(feature_df)
     feature_df.to_csv("data/csv_files/sample_audio_features.csv", index=False)
     train_audio_model()
